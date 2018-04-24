50ae67e992e9615985569aff62b05a07dc49e99f
lexa <sokolov@ozero.net>
2018-04-24 21:29:26 +0300

    Method initialized

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -13,12 +13,21 @@ public class TicTacToe {
         this.board = setBoard();
     }
 
-    public char[] setBoard() {
+    public static char[] setBoard() {
         char[] board = new char[9];
         for (int i = 0; i < board.length; i ++) {
             board[i] = '-';
         }
         return board;
+    }
+
+    public boolean playTurn(int spot) {
+        boolean isValid = withRange(spot) && !isSpotTaken(spot);
+        if (isValid) {
+            board[spot - 1] = currentMarker;
+            currentMarker = (currentMarker == userMarker) ? aiMarker : userMarker;
+        }
+        return isValid;
     }
 
 }
