2f3a078efe400da6a026c1d74c464478943e3735
lexa <sokolov@ozero.net>
2018-04-24 21:07:03 +0300

    Fields added

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,3 +1,10 @@
 public class TicTacToe {
 
+    protected char[] board;
+    protected char userMarker;
+    protected char aiMarker;
+    protected char winner;
+    protected char currentMarker;
+
+
 }
